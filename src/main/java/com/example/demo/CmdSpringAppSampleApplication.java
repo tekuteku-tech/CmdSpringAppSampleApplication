21a7040cb1b3252b40a4c905bb58bc504a4d119b
tekuteku-tech <tekuteku.tech.mail@gmail.com>
2023-01-09 14:21:51 +0900

    改良

--- src/main/java/com/example/demo/CmdSpringAppSampleApplication.java
+++ src/main/java/com/example/demo/CmdSpringAppSampleApplication.java
@@ -2,12 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CmdSpringAppSampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CmdSpringAppSampleApplication.class, args);
+
+        try {
+            // Spring アプリケーションを実行し、コンテクストを取得する
+            ConfigurableApplicationContext context = SpringApplication.run(CmdSpringAppSampleApplication.class, args);
+
+            // Springのコンテナを終了させる
+            int springExitCode = SpringApplication.exit(context, () -> 0);
+
+            // Java VMも終了させる
+            System.exit(springExitCode);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            System.exit(1);
+
+        }
+
     }
 
 }
--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,14 +1,24 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Main implements CommandLineRunner {
+public class Main implements ApplicationRunner {
 
     @Override
-    public void run(String... args) throws Exception {
+
+    public void run(ApplicationArguments args) throws Exception {
+
         System.out.println("ここに実行したい処理を書く");
+
+        int counter = 1;
+        for (String arg : args.getNonOptionArgs()) {
+            System.out.println("引数" + counter + "：" + arg);
+            counter++;
+        }
+
     }
 
 }
